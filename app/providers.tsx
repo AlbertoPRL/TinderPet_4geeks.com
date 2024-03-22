d3b04741da113b3737fe7e42120e47e15fde9238
AlbertoPRL <67210580+AlbertoPRL@users.noreply.github.com>
2024-03-21 22:14:55 -0400

    Commit

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,5 +3,8 @@
 import { ChakraProvider } from '@chakra-ui/react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>
-}
+    return(
+    <>
+        <ChakraProvider>{children}</ChakraProvider>
+    </>
+)}
