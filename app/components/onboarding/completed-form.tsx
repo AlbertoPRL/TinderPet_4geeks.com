1b9e74e55c6e81e5a7861b6c90b13b633e41355a
Rene Ruano <rene.ruano95@gmail.com>
2024-05-03 22:31:33 -0400

    added pet-selector page

--- app/components/onboarding/completed-form.tsx
+++ app/components/onboarding/completed-form.tsx
@@ -1,25 +1,38 @@
 import {
-  Button,
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
   Card,
   CardBody,
-  CardFooter,
-  CardHeader,
-  Heading,
-  Text,
 } from "@chakra-ui/react";
 
 export default function CompletedForm() {
   return (
     <Card h={"full"} w={"100%"} shadow={"none"}>
-      <CardHeader>
-        <Heading fontSize="lg" fontWeight="medium" lineHeight="6">
-          Form submitted successfully
-        </Heading>
-      </CardHeader>
-      <CardBody>
-        <Text>Your form has been submitted successfully</Text>
-        <Text>You will be redirected to the main page in 5 seconds</Text>
-        <Text>Thank you for using TinPet</Text>
+      <CardBody
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Alert
+          status="success"
+          variant="subtle"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          height="200px"
+        >
+          <AlertIcon boxSize="40px" mr={0} />
+          <AlertTitle mt={4} mb={1} fontSize="lg">
+            Form submitted!
+          </AlertTitle>
+          <AlertDescription maxWidth="sm">
+            Your form has been submitted successfully. You will be redirected to
+            the main page in 5 seconds. Thank you for using TinderPet.
+          </AlertDescription>
+        </Alert>
       </CardBody>
     </Card>
   );
